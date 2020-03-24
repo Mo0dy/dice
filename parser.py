d8e1bd5e6976c41269b9e31958803f742efcab72
Felix Muehlenberend <felix.muehlenberend@gmail.com>
2020-03-24 14:08:48 +0100

    Added Syntax trees and parser for syntax trees.

---
+++ parser.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import re
+from syntaxtree import BinOp, Val
+from lexer import Token, Lexer, INTEGER, ROLL, GREATER_THEN, IF_THEN, PLUS, EOF
+
+"""Generates Abstract Syntax Trees"""
+
+
+class Parser(object):
+    """Basic parser funcitonality"""
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = lexer.next_token()
+
+    def exception(self, message=""):
+        raise Exception("Could not parse: {}".format(message))
+
+    def eat(self, type):
+        """Checks for token type and advances token"""
+        if type != self.current_token.type:
+            self.exception("Tried to eat: {} but found {}".format(type, self.current_token.type))
+        self.current_token = self.lexer.next_token()
+
+class DiceParser(Parser):
+    """Parser for the Dice language"""
+    def factor(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return Val(token)
+
+    def roll(self):
+        node = self.factor()
+        if self.current_token.type == ROLL:
+            token = self.current_token
+            self.eat(ROLL)
+            node = BinOp(node, token, self.factor())
+        return node
+
+    def side(self):
+        node = self.roll()
+        while self.current_token.type == PLUS:
+            token = self.current_token
+            self.eat(PLUS)
+            node = BinOp(node, token, self.roll())
+        return node
+
+    def expr(self):
+        node = self.side()
+        if self.current_token.type == GREATER_THEN:
+            # store token for AST
+            token = self.current_token
+            self.eat(GREATER_THEN)
+            node = BinOp(node, token, self.side())
+        return node
+
+if __name__ == "__main__":
+    lexer = Lexer("1d20 + 2 >= 20")
+    parser = DiceParser(lexer)
+    ast = parser.expr()
+    print(ast)
---
+++ syntaxtree.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+class AST(object):
+    """The baseclass for all AST nodes"""
+
+class BinOp(AST):
+    """Node for all binary operators"""
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+    def __repr__(self):
+        result = "BinOp: {}".format(self.op)
+        left_node = str(self.left)
+        right_node = str(self.right)
+        result += '\t|'.join(('\n' + "left: " + str(self.left).lstrip()).splitlines(True))
+        result += '\t|'.join(('\n' + "right: " + str(self.right).lstrip()).splitlines(True))
+        return result
+
+class Val(AST):
+    """Value end node"""
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+    def __repr__(self):
+        return "Val: {}".format(self.value)
