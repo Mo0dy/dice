3f448810292841e153496770664932592472e730
Felix Muehlenberend <felix.muehlenberend@gmail.com>
2020-03-26 22:42:05 +0100

    Added #define to parser with working substitutions

--- dice.py
+++ dice.py
@@ -4,6 +4,7 @@
 
 import sys
 import fileinput
+import re
 
 from interpreter import Interpreter
 from diceengine import ResultList, Distrib
@@ -11,10 +12,6 @@ from parser import DiceParser
 from lexer import Lexer
 
 def interpret(text):
-    if text == "\n":
-        return "\n"
-    if text[0] == '#':
-        return ""
     try:
         result = Interpreter(DiceParser(Lexer(text)).expr()).interpret()
         # round and prettyfy
@@ -33,7 +30,53 @@ def runinteractive():
         print(interpret(text))
     return 2
 
+class Definition(object):
+    def __init__(self, statement):
+        """parse definition and extract information"""
+        regex = r"^([a-zA-Z]\w*)(?:\(([a-zA-Z]\w*(?: *, *[a-zA-A]\w*)*)\))? *(.*)$"
+        match = re.search(regex, statement)
+        if not match:
+            raise Exception("Definition exception. Could not parse: {}".format(statement))
+        self.name = match.group(1)
+        self.attributes = []
+        if match.group(2):
+            self.attributes = [x.strip() for x in match.group(2).split(",")]
+        if not match.group(3):
+            raise Exception("Definition needs a definition body could not parse: {}".format(statement))
+        self.body = match.group(3).strip()
+        self.statement = statement
+
+    def __repr__(self):
+        return "Definition: {}".format(self.statement)
+
+    def parse(self, line):
+        """Parses a line and applies itself wherever possible"""
+        # It's a lot simpler if I make two regexes out of this one with and one without attributes
+        if self.attributes:
+            attr_regex = " *, *".join(['"(.*?)"'] * len(self.attributes))
+            regex = r"{name}\({attributes}\)".format(name=self.name, attributes=attr_regex)
+            new_line = line
+            match = re.search(regex, new_line)
+            while match:
+                # NOTE: this can be an endless loop with a carefully chosen #definition
+                groups = match.groups()
+                replacement = self.body
+                for attribute, value in zip(self.attributes, groups):
+                    replacement = re.sub(attribute, value, replacement)
+                new_line = re.sub(regex, replacement, new_line, count=1)
+                match = re.search(regex, new_line)
+            return new_line
+        else:
+            regex = r"{name}".format(name=self.name)
+            return re.sub(regex, self.body, line)
+
+def define(line, definitions):
+    """Defines a preprocessor statement (replace) similar to c #define"""
+    definitions.append(Definition(line))
+
 def main(args):
+    definitions = []
+
     if len(args) > 1:
         if args[1] in ["-i", "--interactive"]:
             return runinteractive()
@@ -43,10 +86,19 @@ def main(args):
 
     input_lines = fileinput.input()
     for line in input_lines:
-        result = str(interpret(line)).strip()
-        if result:
-            sys.stdout.write("dice> " + line)
-            sys.stdout.write(str(result) + "\n\n")
+        if line == "\n" or line[0] in '!#':
+            if line.startswith("!define"):
+                define(line[len("!define"):].strip(), definitions)
+            if line[0] == '#':
+                sys.stdout.write(line)
+        else:
+            # apply definitions:
+            for d in definitions:
+                line = d.parse(line)
+            result = str(interpret(line))
+            if result:
+                sys.stdout.write("dice> " + line)
+                sys.stdout.write(str(result) + "\n\n")
     return 0
 
 
